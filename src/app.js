ba6e3c11f8dc962c2a015ba0bb291efc9be655ed
Jatin shankar <shankarjatin1005@gmail.com>
2022-11-07 14:47:59 +0530

    interface improved

--- src/app.js
+++ src/app.js
@@ -5,10 +5,6 @@ const port = process.env.PORT || 9000;
 const mongoose = require ("mongoose");
 const DB = "mongodb://localhost:27017/student_management1";
 
-
-
-
-
 app.use(express.static('public'));
 app.use(express.json());
 
@@ -18,10 +14,12 @@ mongoose.connect(DB).then(()=>{
     console.log("connection successful");
 }).catch((err) => console.log("connection failed") );
 
-
 app.get("/students" ,(req,res)=>{
     res.sendFile(__dirname + "/models/index.html");
+ 
 })
+
+
 app.post("/students" , (req,res)=>{
     
     const student_data = new Student({
@@ -40,7 +38,6 @@ app.post("/students" , (req,res)=>{
 })
 
 
-
 // app.post("/students" , (req,res)=>{
 //     console.log(req.body);
 //     const student_data = new Student(req.body);
@@ -52,14 +49,14 @@ app.post("/students" , (req,res)=>{
 // })
 
 
-// app.get("/students" ,async(req,res)=>{
-//     try{
-//       const students_data = await Student.find();
-//       res.send(students_data);
-//     }catch(e){
-//         res.status(400).send(e);
-//     }
-// })
+app.get("/studentsData" ,async(req,res)=>{
+    try{
+      const students_data = await Student.find();
+      res.send(students_data);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
 
 app.get("/student/:roll_no" , async(req,res)=>{
     try{
